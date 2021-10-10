8db2779a62a5803c2bcddc9111ca3b0ecfaa4c07
Md. Rifat Islam <xxxxxxrifatxxxxxx@gmail.com>
2021-10-10 20:38:44 +0600

    Project Done

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA0dOyJihnGXyBb2r-AvcFSJpzWAF-KgFk",
+    authDomain: "ema-john-simple-d6c91.firebaseapp.com",
+    projectId: "ema-john-simple-d6c91",
+    storageBucket: "ema-john-simple-d6c91.appspot.com",
+    messagingSenderId: "1021113285909",
+    appId: "1:1021113285909:web:efd5e042a146b9db967d1c",
+    measurementId: "G-Y5DNXV6SGP"
+};
+
+export default firebaseConfig;
